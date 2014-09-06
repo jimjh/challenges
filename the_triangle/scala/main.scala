9fd24e2e270ee2f03f8b9f1ecac0b73ce680a641
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2014-09-06 13:10:40 -0700

    add scalaz

--- the_triangle/scala/main.scala
+++ the_triangle/scala/main.scala
@@ -4,6 +4,10 @@ object main {
 
   type Row = IndexedSeq[Int]
 
+  private[this] def zeroArray(len: Int) = Array.fill(len)(0)
+  private[this] val ident = identity[Row] _
+
+  /** Solve for all nodes on this row, given results from the row below. */
   private[this] def parse(l: String)(down: Row): Row =
     l.split(' ').zipWithIndex.map {
       case (node, i) =>
@@ -12,24 +16,27 @@ object main {
         Math.max(left, right) + node.toInt
     }
 
-  private[this] def zeroArray(len: Int) = Array.fill(len)(0)
+  /** Syntax sugar for parse, so I can partially apply it. */
   private[this] val max = parse _
-  private[this] val ident = identity[Row] _
 
   def readLnN(n: Int): IO[IndexedSeq[String]] = IO {
-    (1 to n).map { _ => readLine() }
+    1 to n map { _ => readLine() }
+  }
+
+  def solve(lines: IndexedSeq[String]) = {
+    val cont = (ident /: lines) {
+      (f, l) => max(l) andThen f
+    }
+    IO { cont(zeroArray (lines.size+1))(0) }
   }
 
   def program = {
-    val n: IO[Int] = readLn.map(_.toInt)
-    val lines: IO[IndexedSeq[String]] = n.flatMap(readLnN)
-    val ans = lines.map { ls =>
-      val cont = ls.foldLeft(ident) {
-        (fun, line) => max(line) andThen fun
-      }
-      cont(zeroArray (ls.size+1))(0)
-    }
-    ans.flatMap { i:Int => putStrLn(i.toString) }
+    for {
+      n <- readLn
+      lines <- readLnN(n.toInt)
+      ans <- solve(lines)
+      _ <- putLn(ans)
+    } yield()
   }
 
   def main(args: Array[String]) {
